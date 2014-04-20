80b1fa6ffa080085ef6c3b72fe1344eb32b49a66
boo1ean <boo1ean0807@gmail.com>
2014-04-20 21:49:53 +0300

    Toggle req info on click

--- dashboard/list.js
+++ dashboard/list.js
@@ -18,10 +18,21 @@ var prepareData = function(data) {
 	return data;
 };
 
+var toggleInfo = function() {
+	var info = this.querySelector('.info');
+
+	if (info.style.display != 'block') {
+		info.style.display = 'block';
+	} else {
+		info.style.display = 'none';
+	}
+};
+
 module.exports = {
 	append: function(req) {
 		var view = document.createElement('div');
 		view.innerHTML = templates['request'](prepareData(req));
+		view.addEventListener('click', toggleInfo);
 		root.insertBefore(view, root.firstChild);
 	}
 };
