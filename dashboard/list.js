33474746d64b06160961b6e684354df9d0ba304c
boo1ean <boo1ean0807@gmail.com>
2014-04-20 19:37:47 +0300

    Wire up basic requests sync

--- dashboard/list.js
+++ dashboard/list.js
@@ -1,10 +1,25 @@
 var templates = require('./templates');
 var root = document.getElementById('main');
 
+var prepareData = function(data) {
+	var headers = [];
+
+	for (var name in data.headers) {
+		headers.push({
+			name: name,
+			value: data.headers[name]
+		});
+	}
+
+	data.headers = headers;
+
+	return data;
+};
+
 module.exports = {
 	append: function(req) {
 		var view = document.createElement('div');
-		view.innerHTML = templates['request'](req);
+		view.innerHTML = templates['request'](prepareData(req));
 		root.insertBefore(view, root.firstChild);
 	}
 };
