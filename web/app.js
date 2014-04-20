2187f9c15616ea06ae0c4d3d5161a360d287e72e
boo1ean <boo1ean0807@gmail.com>
2014-04-20 19:22:00 +0300

    Wire up socket rooms by path, emit path-related requests to path's room

--- web/app.js
+++ web/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var socketio = require('socket.io');
 var http = require('http');
 var path = require('path');
+var _ = require('lodash');
+var bodyParser = require('body-parser');
 
 var port = 5000;
 var app = express();
@@ -9,12 +11,17 @@ var server = http.createServer(app).listen(port)
 var io = socketio.listen(server);
 var html = path.resolve(__dirname + '/views/index.html');
 
+app.use(bodyParser());
 app.use(express.static(__dirname + '/../static'));
 
 app.all('*', function(req, res) {
+	var payload = _.pick(req, ['headers', 'path', 'body', 'query']);
+	io.sockets.in(req.path).emit('request', payload);
 	res.sendfile(html);
 });
 
 io.sockets.on('connection', function(socket) {
-	socket.emit('news', { hello: 'world' });
+	socket.on('subscribe', function(data) {
+		socket.join(data.room);
+	})
 });
